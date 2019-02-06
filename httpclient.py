e2ce2812bb089a3e67462258923bbf6329426ad3
shelley <stian3@ualberta.ca>
2019-02-06 01:23:29 -0500

    can pass almost all tests

--- httpclient.py
+++ httpclient.py
@@ -23,6 +23,7 @@ import socket
 import re
 # you may use urllib to encode data appropriately
 import urllib.parse
+from urllib.parse import urlparse
 
 def help():
     print("httpclient.py [GET/POST] [URL]\n")
@@ -31,28 +32,41 @@ class HTTPResponse(object):
     def __init__(self, code=200, body=""):
         self.code = code
         self.body = body
+    def __str__(self):
+        return 'CODE: ' + str(self.code) + '\nBODY: ' + self.body
 
 class HTTPClient(object):
     def get_host_port(self,url):
         purl = urlparse(url)
-        return (purl.hostname, purl.port)
+        host = purl.hostname
+        port = purl.port
+        if port == None:
+            port = 80
+        return host, port
 
     def connect(self, host, port):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((host, port))
-        data = self.recvall(self.socket)
-        print("DATA = ", data)
-        return data
+        return None
 
     def get_code(self, data):
-
-        return None #return code
+        code = data.split('\r\n')[0].split(' ')[1]
+        # headers = self.get_headers(data)
+        # code = headers.split('\r\n')[0].split(' ')[1]
+        # print("****CODE:" , code)
+        return int(code)
 
     def get_headers(self, data):
-        return None #return headers
+        split_data = data.split('\r\n\r\n')
+        headers = split_data[0]
+        # print("*****HEADERS", headers)
+        return headers
 
     def get_body(self, data):
-        return None #return body
+        split_data = data.split('\r\n\r\n')[1:]
+        body = ('\r\n').join(split_data)
+        # print("*****BODY", body)
+        return body 
 
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
@@ -70,24 +84,96 @@ class HTTPClient(object):
                 buffer.extend(part)
             else:
                 done = not part
+        print("BUFFER: <<\n", buffer, ">>\n")
         return buffer.decode('utf-8')
 
     def GET(self, url, args=None):
         code = 500
         body = ""
-        response = ""
+
 		#parse url
         purl = urlparse(url)
-		#check scheme
-		if(purl.scheme == "http"):
-			#DO MORE stuff
-            self.connect(self.get_host_port(url))
-        self.sendall(response)
+
+        #get host and port
+        host, port = self.get_host_port(url)
+
+        #connect
+        self.connect(host,port)
+
+        #create request
+        if purl.path == '':
+            request = 'GET / HTTP/1.1\r\n'
+        else:
+            request = 'GET ' + purl.path + ' HTTP/1.1\r\n'
+        request += ('Host: ' + purl.hostname + "\r\n")
+        request += ('Accept: */*\r\n')
+        request += ('Connection: Close\r\n\r\n')
+        print(request)
+
+        #send request
+        self.sendall(request)
+
+        #get response
+        try:
+            response = self.recvall(self.socket)
+            code = self.get_code(response)
+            body = self.get_body(response)
+            print("CODE:\n", code)
+            print("BODY:\n", body)
+        except Exception as e:
+            print(e)
+            code = 500
+            body = "An error occured during GET request"
+
+        #close connection
+        self.close()
+
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
         code = 500
         body = ""
+        post_data = str(args)
+
+        #parse url
+        purl = urlparse(url)
+
+        #get host and port
+        host, port = self.get_host_port(url)
+
+        #connect
+        self.connect(host, port)
+
+        #create request
+        if purl.path == '':
+            request = 'POST / HTTP/1.1\r\n'
+        else:
+            request = 'POST ' + purl.path + ' HTTP/1.1\r\n'
+        request += ('Host: ' + purl.hostname + "\r\n")
+        request += ('Accept: */*\r\n')
+        request += ('Content-Length: ' + str(len(post_data)) + '\r\n')
+        request += ('Connection: Close\r\n\r\n')
+        request += (post_data + '\r\n\r\n')
+        print("REQUEST:\n", request, "---")
+
+        #send request
+        self.sendall(request)
+
+        #get response
+        try:
+            response = self.recvall(self.socket)
+            code = self.get_code(response)
+            body = self.get_body(response)
+            print("CODE:\n", code)
+            print("BODY:\n", body)
+        except Exception as e:
+            print(e)
+            code = 500
+            body = "An error occured during POST request"
+
+        #close connection
+        self.close()
+
         return HTTPResponse(code, body)
 
     def command(self, url, command="GET", args=None):
